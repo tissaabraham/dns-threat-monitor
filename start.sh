31ec32b4954d341869a5275a02100d7b83892d59
Stephen <S.Small6@universityofgalway.ie>
2026-06-20 23:04:29 +0100

    Some fixes

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ dnsmasq --no-daemon &
 # Give dnsmasq a moment to start up and create the log file
 sleep 2
 
+# Creates the db file if it doesn't already exist (Stops the compose from crashing if not there)
+touch /app/dns_threat_monitor.db &
+
 # Start the dashboard
 python dashboard/app.py &
 
